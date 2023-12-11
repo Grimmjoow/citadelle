8fc4dafdc4d1969c310fe8a2667795234cf17d1f
ODeimosO <127611283+ODeimosO@users.noreply.github.com>
2023-12-11 01:28:15 +0100

    Ajout des classes Voyante et Reine + modifcation des anciennnes classes pour les adapter

--- src/modele/Roi.java
+++ src/modele/Roi.java
@@ -1,29 +1,71 @@
+// Package "modele"
 package modele;
 
-public class Roi extends Personnage{
-	//constructeur
-	public Roi() {
-		this.nom="Roi";
-		this.rang=4;
-		this.caracteristiques=Caracteristiques.ROI;
-	}
-	//m�thodes
-	public void utiliserPouvoir() {
-		if(this.joueur!=null && this.assassine==false) {
-			System.out.println("Je prends la courronne");
-			getJoueur().setPossedeCouronne(true);
-		}		
-	}
-	public void percevoirRessourcesSpecifiques() {
-		if(this.joueur!=null && this.assassine==false) {
-			int compte=0;
-			for(int i=0;i<getJoueur().nbQuartiersDansCite();i++) {
-				if(getJoueur().getQuartier(i).getType()=="NOBLE") {//getQuartier cible un Quartier dans le tableau Cite du Joueur
-					compte++;
-				}
-			}
-			this.joueur.ajouterPieces(compte);
-			System.out.println("Vous avez "+compte+" pieces dans votre tr�sor !");
-		}
-	}
+// Déclaration de la classe "Roi" qui étend la classe "Personnage"
+public class Roi extends Personnage {
+
+    // Constructeur de la classe "Roi"
+    public Roi() {
+        super("Roi", 4, Caracteristiques.ROI);
+    }
+
+    // Méthode pour utiliser le pouvoir du roi
+    public void utiliserPouvoir() {
+        if (!this.getAssassine()) {
+            if (this.getJoueur() != null) {
+                System.out.println("Je prends la couronne");
+                this.getJoueur().setPossedeCouronne(true);
+            } else {
+                System.out.println("Le joueur n'est pas défini");
+            }
+        } else {
+            System.out.println("Vous ne pouvez pas utiliser votre pouvoir");
+        }
+    }
+
+    // Méthode pour percevoir les ressources spécifiques du roi
+    @Override
+    public void percevoirRessourcesSpecifiques() {
+        if (this.getAssassine()) {
+            super.percevoirRessourcesSpecifiques();
+        } else {
+            int piecesGagnees = 0;
+            if (this.getJoueur() != null) {
+                for (int i = 0; i < this.getJoueur().nbQuartiersReelDansCite(); i++) {
+                    if (this.getJoueur().getCite()[0] != null) {
+                        if (this.getJoueur().getCite()[i].getType().equals("NOBLE")) {
+                            piecesGagnees += 1;
+                        }
+                    }
+                }
+                if (this.getJoueur().getPossedeEcoleMag()) {
+                    // La merveille École de Magie change de type à la perception des ressources
+                    piecesGagnees += 1;
+                }
+                this.getJoueur().ajouterPieces(piecesGagnees);
+            }
+            System.out.println(piecesGagnees + " pièce(s) ajoutée(s)");
+        }
+    }
+
+    // Méthode pour utiliser le pouvoir du roi (avatar)
+    @Override
+    public void utiliserPouvoirAvatar() {
+        if (!this.getAssassine()) {
+            if (this.getJoueur() != null) {
+                System.out.println("Je prends la couronne");
+                this.getJoueur().setPossedeCouronne(true);
+            } else {
+                System.out.println("Le joueur n'est pas défini");
+            }
+        } else {
+            System.out.println("Vous ne pouvez pas utiliser votre pouvoir");
+        }
+    }
+
+    // Méthode pour l'activation du pouvoir de la sorcière (non utilisée pour le roi)
+    @Override
+    public void activationPouvoirSorciere(Joueur joueurSorciere) {
+        // TODO Auto-generated method stub
+    }
 }
--- src/modele/Voleur.java
+++ src/modele/Voleur.java
@@ -1,40 +1,87 @@
+// Package "modele"
 package modele;
 
-import controleur.Interaction;
+// Import des classes nécessaires
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
-public class Voleur extends Personnage{
-	//constructeur
-	public Voleur() {
-		this.nom="Voleur";
-		this.rang=2;
-		this.caracteristiques=Caracteristiques.VOLEUR;
-	}
-	//m�thode
-	public void utiliserPouvoir() {
-		int i=1;
-		int lecture=0;
-		System.out.println("Quel personnage voulez-vous voler ?");
-		while(i<=this.getPlateau().getNombrePersonnages()) {
-			System.out.println(i+" "+this.getPlateau().getPersonnage(i-1).getNom());
-			i++;
-		}
-		System.out.print("Votre choix : ");
-		boolean continu=true;
-		do {
-			try {
-				lecture=Interaction.lireUnEntier();
-				if(this.getPlateau().getPersonnage(lecture-1).getRang()<=2) {
-					throw new Exception();
-				} else {
-					this.getPlateau().getPersonnage(lecture-1).setVole();
-					this.getJoueur().ajouterPieces(this.getPlateau().getJoueur(lecture-1).nbPieces());
-					this.getPlateau().getJoueur(lecture-1).retirerPieces(this.getJoueur().nbPieces());
-					continu=false;
-				}
-			}catch (Exception e){
-				System.out.println("Vous ne pouvez pas vous voler ou voler l'assassin.");
-				System.out.print("Votre choix : ");
-			}
-		} while (continu);			
-	}
-}
+import Exception.RangException;
+import Exception.SelfChosen;
+
+// Déclaration de la classe "Voleur" qui étend la classe "Personnage"
+public class Voleur extends Personnage {
+
+    // Constructeur de la classe "Voleur"
+    public Voleur() {
+        super("Voleur", 2, Caracteristiques.VOLEUR);
+    }
+
+    // Méthode pour utiliser le pouvoir du voleur
+    @Override
+    public void utiliserPouvoir() {
+        // Vérifie si le personnage est assassiné
+        if (this.getAssassine()) {
+            System.out.println("Votre personnage a été assassiné");
+        } else {
+            boolean continu = true;
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Quel personnage voulez-vous voler ?");
+
+            // Affiche la liste des personnages disponibles
+            for (int i = 0; i < this.getPlateau().getNombrePersonnages(); i++) {
+                System.out.println((i + 1) + "." + this.getPlateau().getPersonnage(i).getNom());
+            }
+
+            do {
+                System.out.println("Votre choix : ");
+                try {
+                    int j = sc.nextInt();
+                    int k = j - 1;
+
+                    // Vérifie si le joueur tente de se voler lui-même
+                    if (this.getPlateau().getPersonnage(k).equals(this)) {
+                        throw new SelfChosen();
+                    } else if (this.getPlateau().getPersonnage(k).getRang() == 1) {
+                        throw new RangException();
+                    }
+
+                    // Active le pouvoir de voler sur le personnage choisi
+                    this.getPlateau().getPersonnage(k).setVole();
+                    // Gérer les pièces ??
+
+                    System.out.println(this.getPlateau().getPersonnage(k).getNom() + " a été volé.");
+                    continu = false;
+                } catch (SelfChosen e) {
+                    System.out.println("Vous ne pouvez pas vous voler");
+                } catch (RangException e) {
+                    System.out.println("Vous ne pouvez pas choisir un personnage de rang 1");
+                }
+            } while (continu);
+        }
+    }
+
+    // Méthode pour utiliser le pouvoir du voleur (avatar)
+    @Override
+    public void utiliserPouvoirAvatar() {
+        // Vérifie si le personnage est assassiné
+        if (this.getAssassine()) {
+            System.out.println("Votre personnage a été assassiné");
+        } else {
+            boolean continu = true;
+            System.out.println("Quel personnage voulez-vous voler ?");
+
+            // Affiche la liste des personnages disponibles
+            for (int i = 0; i < this.getPlateau().getNombrePersonnages(); i++) {
+                System.out.println((i + 1) + "." + this.getPlateau().getPersonnage(i).getNom());
+            }
+
+            do {
+                System.out.println("Votre choix : ");
+                try {
+                    int j = ThreadLocalRandom.current().nextInt(0, this.getPlateau().getNombrePersonnages()) + 1;
+                    int k = j - 1;
+
+                    // Vérifie si le joueur tente de se voler lui-même
+                    if (this.getPlateau().getPersonnage(k).equals(this)) {
+                        throw new SelfChosen();
+                    } else if (this.getPlate
