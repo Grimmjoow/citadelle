8d6e80f0fc8476a2f169385969ab5f78b6ff0077
ODeimosO <127611283+ODeimosO@users.noreply.github.com>
2023-12-11 02:01:07 +0100

    Classe joueur terminée, beaucoup de modification pour l'adapter aux quartiers+regles

--- src/modele/Joueur.java
+++ src/modele/Joueur.java
@@ -6,25 +6,25 @@ import java.util.Random;
 import application.Configuration;
 
 public class Joueur {
-    private String nom;
-    private int tresor;
-    private Quartier[] cite;
-    private int nbQuartiersReel;
-    private int nbQuartiersMonument;
-    private ArrayList<Quartier> main;
-    private boolean possedeCouronne;
-    protected Personnage monPersonnage;
-    protected Personnage[] mesPersonnages;
-    private boolean aFini;
-    private int points;
-    private boolean possedeManu;
-    private boolean possedeCarriere;
-    private boolean possedeEcoleMag;
+    private String nom;                 // Nom du joueur
+    private int tresor;                 // Nombre de pièces du joueur
+    private Quartier[] cite;            // Quartiers construits dans la cité du joueur
+    private int nbQuartiersReel;        // Nombre de quartiers réels dans la cité du joueur
+    private int nbQuartiersMonument;    // Nombre de quartiers monument dans la cité du joueur
+    private ArrayList<Quartier> main;   // Quartiers dans la main du joueur
+    private boolean possedeCouronne;    // Indique si le joueur possède la couronne
+    protected Personnage monPersonnage; // Personnage associé au joueur
+    protected Personnage[] mesPersonnages; // Tableau des personnages du joueur
+    private boolean aFini;              // Indique si le joueur a fini son tour
+    private int points;                 // Points du joueur
+    private boolean possedeManu;        // Indique si le joueur possède le Manoir
+    private boolean possedeCarriere;    // Indique si le joueur possède la Carrière
+    private boolean possedeEcoleMag;    // Indique si le joueur possède l'École de Magie
 
-    // Constructeur pour la classe Joueur
-    public Joueur(String nom){
+    // Constructeur
+    public Joueur(String nom) {
         this.nom = nom;
-        this.tresor  = 0;
+        this.tresor = 0;
         this.nbQuartiersReel = 0;
         this.possedeCouronne = false;
         this.possedeCarriere = false;
@@ -32,149 +32,236 @@ public class Joueur {
         this.nbQuartiersMonument = 0;
 
         this.cite = new Quartier[8];
-        this.main = new ArrayList<Quartier>();
+        this.main = new ArrayList<>();
         this.monPersonnage = null;
         this.mesPersonnages = new Personnage[2];
     }
 
-    // Méthode pour définir si le joueur possède l'Ecole de Magie
-    public void setPossedeEcoleMag(boolean bool){
+    // Méthode pour définir si le joueur possède l'École de Magie
+    public void setPossedeEcoleMag(boolean bool) {
         this.possedeEcoleMag = bool;
     }
 
-    // Méthode pour obtenir l'information si le joueur possède l'Ecole de Magie
-    public boolean getPossedeEcoleMag(){
+    // Méthode pour obtenir si le joueur possède l'École de Magie
+    public boolean getPossedeEcoleMag() {
         return this.possedeEcoleMag;
     }
 
-    // Méthode pour obtenir l'information si le joueur possède la Manufacture
+    // Méthode pour obtenir si le joueur possède le Manoir
     public boolean getPossedeManu() {
         return possedeManu;
     }
 
-    // Méthode pour définir si le joueur possède la Manufacture
+    // Méthode pour définir si le joueur possède le Manoir
     public void setPossedeManu(boolean possedeManu) {
         this.possedeManu = possedeManu;
     }
 
     // Méthode pour définir si le joueur possède la Carrière
-    public void setPossedeCarriere(boolean bool){
+    public void setPossedeCarriere(boolean bool) {
         this.possedeCarriere = bool;
     }
 
-    // Méthode pour obtenir l'information si le joueur possède la Carrière
-    public boolean getPossedeCarriere(){
+    // Méthode pour obtenir si le joueur possède la Carrière
+    public boolean getPossedeCarriere() {
         return this.possedeCarriere;
     }
 
-    // Méthode pour obtenir le nombre de points du joueur
-    public int getPoints(){
+    // Méthode pour obtenir les points du joueur
+    public int getPoints() {
         return this.points;
     }
 
-    // Méthode pour définir le nombre de points du joueur
-    public void setPoints(int points){
+    // Méthode pour définir les points du joueur
+    public void setPoints(int points) {
         this.points = points;
     }
 
-    // Méthode pour obtenir l'information si le joueur a terminé son tour
-    public boolean getAFini(){
+    // Méthode pour obtenir si le joueur a fini son tour
+    public boolean getAFini() {
         return this.aFini;
     }
 
-    // Méthode pour définir si le joueur a terminé son tour
-    public void setAFini(boolean choix){
+    // Méthode pour définir si le joueur a fini son tour
+    public void setAFini(boolean choix) {
         this.aFini = choix;
     }
 
     // Méthode pour obtenir le nom du joueur
-    public String getNom(){
+    public String getNom() {
         return this.nom;
     }
 
     // Méthode pour obtenir le nombre de pièces du joueur
-    public int nbPieces(){
+    public int nbPieces() {
         return this.tresor;
     }
 
     // Méthode pour obtenir le nombre de quartiers réels dans la cité du joueur
-    public int nbQuartiersReelDansCite(){
+    public int nbQuartiersReelDansCite() {
         return this.nbQuartiersReel;
     }
 
     // Méthode pour obtenir le nombre de quartiers monument dans la cité du joueur
-    public int nbQuartiersMonumentDansCite(){
+    public int nbQuartiersMonumentDansCite() {
         return this.nbQuartiersReel;
     }
 
     // Méthode pour obtenir les quartiers dans la cité du joueur
-    public Quartier[] getCite(){
+    public Quartier[] getCite() {
         return this.cite;
     }
 
     // Méthode pour obtenir les quartiers dans la main du joueur
-    public ArrayList<Quartier> getMain(){
+    public ArrayList<Quartier> getMain() {
         return this.main;
     }
 
-    // Méthode pour obtenir le nombre de quartiers réels dans la main du joueur
-    public int nbQuartiersReelDansMain(){
+    // Méthode pour obtenir le nombre de quartiers dans la main du joueur
+    public int nbQuartiersReelDansMain() {
         return this.main.size();
     }
 
-    // Méthode pour obtenir l'information si le joueur possède la Couronne
-    public boolean getPossedeCouronne(){
+    // Méthode pour obtenir si le joueur possède la couronne
+    public boolean getPossedeCouronne() {
         return this.possedeCouronne;
     }
 
-    // Méthode pour définir si le joueur possède la Couronne
-    public void setPossedeCouronne(boolean b){
+    // Méthode pour définir si le joueur possède la couronne
+    public void setPossedeCouronne(boolean b) {
         this.possedeCouronne = b;
     }
 
     // Méthode pour obtenir le personnage du joueur
-    public Personnage getPersonnage(){
+    public Personnage getPersonnage() {
         return this.monPersonnage;
     }
 
-    // Méthode pour ajouter des pièces au trésor du joueur
-    public void ajouterPieces(int nb){
-        if(nb >= 0){
+    // Méthode pour ajouter des pièces au joueur
+    public void ajouterPieces(int nb) {
+        if (nb >= 0) {
             this.tresor += nb;
         } else {
-            System.out.println("Le nombre de pièces passé en paramètre n'est pas valide");
+            System.out.println("Le nombre de pièces passé en paramètre n'est pas valable");
         }
     }
 
-    // Méthode pour retirer des pièces du trésor du joueur
-    public void retirerPieces(int nb){
-        if(nb >= 0){
-            if(tresor - nb < 0){
+    // Méthode pour retirer des pièces au joueur
+    public void retirerPieces(int nb) {
+        if (nb >= 0) {
+            if (tresor - nb < 0) {
                 System.out.println("Trop grand nombre de pièces");
             } else {
                 this.tresor -= nb;
             }
         } else {
-            System.out.println("Le nombre de pièces passé en paramètre n'est pas valide");
+            System.out.println("Le nombre de pièces passé en paramètre n'est pas valable");
         }
     }
 
     // Méthode pour ajouter un quartier dans la cité du joueur
-    public void ajouterQuartierDansCite(Quartier quartier){ 
-        for(int i = 0; i < 8; i++){
-            if(cite[i] == null){
-              cite[i] = quartier;
-              break;  
+    public void ajouterQuartierDansCite(Quartier quartier) {
+        for (int i = 0; i < 8; i++) {
+            if (cite[i] == null) {
+                cite[i] = quartier;
+                break;
             }
         }
-        if(nbQuartiersMonument < 8 && nbQuartiersReel < 8){
+        if (nbQuartiersMonument < 8 && nbQuartiersReel < 8) {
             cite[nbQuartiersReel] = quartier;
             nbQuartiersReel += 1;
             nbQuartiersMonument += 1;
 
-            // Monument
-            if(quartier.equals(Configuration.monument)){
+            // Gestion des quartiers Monument
+            if (quartier.equals(Configuration.monument)) {
                 nbQuartiersMonument++;
             }
-            // Monument
+        } else {
+            System.out.println("Nombre de quartiers déjà à 8");
         }
+    }
+
+    // Méthode pour vérifier si un quartier est présent dans la cité du joueur
+    public boolean quartierPresentDansCite(String nomQuartier) {
+        boolean condition = false;
+        for (int i = 0; i < nbQuartiersReel; i++) {
+            if (cite[i] != null && nomQuartier.equals(cite[i].getNom())) {
+                condition = true;
+            }
+        }
+        return condition;
+    }
+
+    // Méthode pour retirer un quartier de la cité du joueur
+    public Quartier retirerQuartierDansCite(String nomQuartier) {
+        boolean condition = false;
+        for (int i = 0; i < 8; i++) {
+            if (cite[i] != null && nomQuartier.equals(cite[i].getNom())) {
+                Quartier quartier = cite[i];
+                cite[i] = null;
+                condition = true;
+                nbQuartiersReel -= 1;
+                nbQuartiersMonument -= 1;
+
+                // Gestion des quartiers Monument
+                if (nomQuartier.equals("Monument")) {
+                    nbQuartiersMonument--;
+                }
+
+                // Réorganisation des quartiers dans la cité
+                for (int j = 1; j < 8; j++) {
+                    if (cite[j - 1] == null) {
+                        cite[j - 1] = cite[j];
+                        cite[j] = null;
+                    }
+                }
+                return quartier;
+            }
+        }
+        return null;
+    }
+
+    // Méthode pour ajouter un quartier dans la main du joueur
+    public void ajouterQuartierDansMain(Quartier quartier) {
+        // Quand la pioche est vide, on ajoute un élément null dans la main, on n'en veut pas
+        if (!(quartier == null)) {
+            main.add(quartier);
+        }
+    }
+
+    // Méthode pour retirer un quartier aléatoire de la main du joueur
+    public Quartier retirerQuartierDansMain() {
+        Random generateur = new Random();
+        int numeroHasard = generateur.nextInt(this.nbQuartiersReelDansMain());
+        if (main.size() == 0) {
+            return null;
+        } else {
+            Quartier quartier = main.get(numeroHasard);
+            main.remove(numeroHasard);
+            return quartier;
+        }
+    }
+
+    // Méthode pour retirer un quartier spécifique de la main du joueur
+    public Quartier retirerQuartierDansMain(Quartier quartier) {
+        if (main.size() == 0) {
+            return null;
+        } else {
+            main.remove(quartier);
+            return quartier;
+        }
+    }
+
+    // Méthode pour réinitialiser les données du joueur
+    public void reinitialiser() {
+        this.tresor = 0;
+        this.main.clear();
+        for (int i = 0; i < 8; i++) {
+            cite[i] = null;
+        }
+        this.nbQuartiersReel = 0;
+        this.nbQuartiersMonument = 0;
+        this.setAFini(false);
+        this.setPoints(0);
+    }
+}
--- src/modele/Quartier.java
+++ src/modele/Quartier.java
@@ -1,69 +1,105 @@
 package modele;
 
 public class Quartier {
-	//attributs
-	private String nom, type, caracteristiques;
-	private int coutConstruction;
-	public static final String[] TYPE_QUARTIERS = {"RELIGIEUX", "MILITAIRE", "NOBLE", "COMMERCANT", "MERVEILLE"};
-	//constructeurs
-	public Quartier() {
-		//initialisation
-		this.setNom("");
-		this.setType("");
-		this.setCout(0);
-		this.setCaracteristiques("");
-	}
-	public Quartier(String nom,String type,int cout,String caracteristiques) {
-		this.setNom(nom);
-		this.setType(type);
-		this.setCout(cout);
-		this.setCaracteristiques(caracteristiques);
-	}
-	public Quartier(String nom,String type,int cout) {
-		this.setNom(nom);
-		this.setType(type);
-		this.setCout(cout);
-		this.setCaracteristiques("");
-	}
-	//accesseurs NOM
-	public String getNom(){
-		return this.nom;
-	}
-	public void setNom(String nom){
-		this.nom = nom;
-	}
-	//accesseurs TYPE
-	public String getType() {
-		return this.type;
-	}
-	public void setType(String type) {
-		int i=0;
-		while(i<TYPE_QUARTIERS.length && this.type!=type) {
-			if(type==TYPE_QUARTIERS[i]) {
-				this.type=type;
-			} else {
-				this.type="";
-			}
-			i++;
-		}
-	}
-	//accesseurs COUT
-	public int getCout() {
-		return this.coutConstruction;
-	}
-	public void setCout(int cout) {
-		if(cout>=1 && cout<=6) {
-			this.coutConstruction=cout;
-		} else {
-			this.coutConstruction=0;
-		}
-	}
-	//accesseurs CARACTERISTIQUES
-	public String getCaracteristiques() {
-		return this.caracteristiques;
-	}
-	public void setCaracteristiques(String caracteristiques) {
-		this.caracteristiques=caracteristiques;
-	}
-}
+    private String nom = "";
+    private String type = "";
+    private String caracteristiques = "";
+    private int coutConstruction = 0;
+    public static final String[] TYPE_QUARTIERS  = {"RELIGIEUX", "MILITAIRE", "NOBLE", "COMMERCANT", "MERVEILLE"};
+    private boolean embelli;
 
+    // Méthode pour obtenir l'information si le quartier est embelli
+    public boolean getEmbelli() {
+        return embelli;
+    }
+
+    // Méthode pour définir si le quartier est embelli
+    public void setEmbelli(boolean embelli) {
+        this.embelli = embelli;
+    }
+
+    // Méthode pour obtenir le nom du quartier
+    public String getNom(){
+        return this.nom;
+    }
+
+    // Méthode pour définir le nom du quartier
+    public void setNom(String nom){
+        this.nom = nom;
+    }
+
+    // Méthode pour obtenir le type du quartier
+    public String getType(){
+        return this.type;
+    }
+
+    // Méthode pour définir le type du quartier
+    public void setType(String type){
+        boolean condition = false;
+        for(int i=0; i<5; i++){
+            if(type == TYPE_QUARTIERS[i]){
+                this.type = type;
+                condition = true;
+            }
+        }
+        if(condition == false){
+            this.type = "";
+        }
+    }
+
+    // Méthode pour obtenir les caractéristiques du quartier
+    public String getCaracteristiques(){
+        return this.caracteristiques;
+    }
+
+    // Méthode pour définir les caractéristiques du quartier
+    public void setCaracteristiques(String caracteristiques){
+        this.caracteristiques = caracteristiques;
+    }
+
+    // Méthode pour obtenir le coût de construction du quartier
+    public int getCout(){
+        return this.coutConstruction;
+    }
+
+    // Méthode pour définir le coût de construction du quartier
+    public void setCout(int coutConstruction){
+        if(coutConstruction <= 6 && coutConstruction >= 1){
+            this.coutConstruction = coutConstruction;
+        } else {
+            this.coutConstruction = 0;
+        }
+    }
+
+    // Méthode pour embellir le quartier
+    public void embellir(){
+        if(!this.getEmbelli()){
+            this.coutConstruction += 1;
+            this.setEmbelli(true);
+            System.out.println("Le quartier a bien été embelli");
+        } else {
+            System.out.println("Quartier déjà embelli");
+        }
+    }
+
+    // Constructeur par défaut
+    public Quartier(){
+    }
+
+    // Constructeur avec nom, type et coût de construction
+    public Quartier(String nom, String type, int cout){
+        setNom(nom);
+        setType(type);
+        setCout(cout);
+        setEmbelli(false);
+    }
+
+    // Constructeur avec nom, type, coût de construction et caractéristiques
+    public Quartier(String nom, String type, int cout, String caracteristiques){
+        setNom(nom);
+        setType(type);
+        setCout(cout);
+        setCaracteristiques(caracteristiques);
+        setEmbelli(false);
+    }
+}
