f84d55a0b0a613643a5b70ebb685d033358e16f8
ODeimosO <127611283+ODeimosO@users.noreply.github.com>
2023-12-11 04:42:03 +0100

    Ajout des derniers personnages de l'extension

--- src/modele/Empereur.java
+++ src/modele/Empereur.java
@@ -1,5 +1,74 @@
 package modele;
 
-public class Empereur {
-    
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+
+import Controleur.Interaction;
+
+public class Empereur extends Personnage {
+
+    // Constructeur par défaut de la classe Empereur
+    public Empereur() {
+        super("Empereur", 4, Caracteristiques.EMPEREUR);
+    }
+
+    // Méthode pour utiliser le pouvoir du personnage Empereur
+    @Override
+    public void utiliserPouvoir() {
+        // Liste pour stocker les joueurs éligibles à recevoir la couronne
+        ArrayList<Joueur> listeJoueur = new ArrayList<Joueur>();
+
+        // Parcourt tous les joueurs sur le plateau
+        for(int i = 0; i < this.getPlateau().getNombreJoueurs(); i++) {
+            // Si le joueur possède déjà la couronne, elle lui est retirée
+            if(this.getPlateau().getJoueur(i).getPossedeCouronne()) {
+                this.getPlateau().getJoueur(i).setPossedeCouronne(false);
+            }
+            // Si le joueur n'est pas l'Empereur lui-même, il est éligible à recevoir la couronne
+            else if(!this.getPlateau().getJoueur(i).getNom().equals(this.getJoueur().getNom())) {
+                listeJoueur.add(this.getPlateau().getJoueur(i));
+            }
+        }
+
+        System.out.println("Veuillez choisir à qui vous voulez donner la couronne");
+        // Affiche la liste des joueurs éligibles
+        for(int j = 0; j < listeJoueur.size(); j++) {
+            System.out.println((j + 1) + " - " + listeJoueur.get(j).getNom());
+        }
+
+        // Demande au joueur de choisir le destinataire de la couronne
+        int choix = Interaction.lireUnEntier(1, listeJoueur.size() + 1) - 1;
+        System.out.println("Vous avez choisi " + listeJoueur.get(choix).getNom() + ", il va recevoir la couronne");
+        listeJoueur.get(choix).setPossedeCouronne(true);
+    }
+
+    // Méthode pour utiliser le pouvoir spécial de l'Empereur avec un choix aléatoire du destinataire de la couronne
+    @Override
+    public void utiliserPouvoirAvatar() {
+        // Liste pour stocker les joueurs éligibles à recevoir la couronne
+        ArrayList<Joueur> listeJoueur = new ArrayList<Joueur>();
+
+        // Parcourt tous les joueurs sur le plateau
+        for(int i = 0; i < this.getPlateau().getNombreJoueurs(); i++) {
+            // Si le joueur possède déjà la couronne, elle lui est retirée
+            if(this.getPlateau().getJoueur(i).getPossedeCouronne()) {
+                this.getPlateau().getJoueur(i).setPossedeCouronne(false);
+            }
+            // Si le joueur n'est pas l'Empereur lui-même, il est éligible à recevoir la couronne
+            else if(!this.getPlateau().getJoueur(i).getNom().equals(this.getJoueur().getNom())) {
+                listeJoueur.add(this.getPlateau().getJoueur(i));
+            }
+        }
+
+        // Choix aléatoire du destinataire de la couronne
+        int choix = ThreadLocalRandom.current().nextInt(0, listeJoueur.size());
+        System.out.println("Vous avez choisi " + listeJoueur.get(choix).getNom() + ", il va recevoir la couronne");
+        listeJoueur.get(choix).setPossedeCouronne(true);
+    }
+
+    // Méthode d'activation du pouvoir de la sorcière (non implémentée ici)
+    @Override
+    public void activationPouvoirSorciere(Joueur joueurSorciere) {
+        // TODO Auto-generated method stub
+    }
 }
--- src/modele/Patricien.java
+++ src/modele/Patricien.java
@@ -1,5 +1,69 @@
 package modele;
 
-public class Patricien {
-    
+public class Patricien extends Personnage {
+
+    // Constructeur par défaut de la classe Patricien
+    public Patricien(){
+        super("Patricien", 4, Caracteristiques.PATRICIEN);
+    }
+
+    // Méthode pour utiliser le pouvoir du Patricien
+    public void utiliserPouvoir(){
+        if(!this.getAssassine()){
+            if(this.getJoueur() != null){
+                System.out.println("Je prends la couronne");
+                this.getJoueur().setPossedeCouronne(true);
+            }else{
+                System.out.println("Le joueur n'est pas défini");
+            }
+        }else{
+            System.out.println("Vous ne pouvez pas utiliser votre pouvoir");
+        }
+    }
+
+    // Méthode pour percevoir des ressources spécifiques en fonction des quartiers dans la cité
+    @Override
+    public void percevoirRessourcesSpecifiques(){
+        if(this.getAssassine()){
+            super.percevoirRessourcesSpecifiques();
+        }else{
+            int count = 0;
+            if(this.getJoueur() != null){
+                for(int i = 0; i < this.getJoueur().nbQuartiersReelDansCite(); i++){
+                    if(this.getJoueur().getCite()[i] != null && this.getJoueur().getCite()[i].getType().equals("NOBLE")){
+                        this.getJoueur().ajouterQuartierDansMain(this.getPlateau().getPioche().piocher());
+                        count++;
+                    }
+                }
+                
+                // Ajout d'une carte supplémentaire si le joueur possède l'école de magie
+                if(this.getJoueur().getPossedeEcoleMag()){
+                    this.getJoueur().ajouterQuartierDansMain(this.getPlateau().getPioche().piocher());
+                    count++;
+                }
+            }
+            System.out.println(count + " quartier(s) ajouté(s) dans la main");
+        }
+    }
+
+    // Méthode pour utiliser le pouvoir spécial du Patricien (identique à utiliserPouvoir)
+    @Override
+    public void utiliserPouvoirAvatar() {
+        if(!this.getAssassine()){
+            if(this.getJoueur() != null){
+                System.out.println("Je prends la couronne");
+                this.getJoueur().setPossedeCouronne(true);
+            }else{
+                System.out.println("Le joueur n'est pas défini");
+            }
+        }else{
+            System.out.println("Vous ne pouvez pas utiliser votre pouvoir");
+        }
+    }
+
+    // Méthode d'activation du pouvoir de la sorcière (non implémentée ici)
+    @Override
+    public void activationPouvoirSorciere(Joueur joueurSorciere) {
+        // TODO Auto-generated method stub
+    }
 }
